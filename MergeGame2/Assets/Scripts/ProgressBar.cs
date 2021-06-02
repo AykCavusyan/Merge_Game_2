3684bd0a58d8902720c0f4078e30324a94840c2b
QBOMONTI\Sebastian <ayk@quantheque.com>
2021-06-03 01:04:34 +0300

    Shader Graph Animation To the Health bar

--- MergeGame2/Assets/Scripts/ProgressBar.cs
+++ MergeGame2/Assets/Scripts/ProgressBar.cs
@@ -5,46 +5,55 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    [SerializeField] Image _renderer;
+    [Range(0, 1)] [SerializeField] float _currentHealth;
+
     private RectTransform barSprite;
-    private float progressAmountPercentage;
-    private float progressBarCurrent;
-    private float lerpSpeed; 
 
-    private float progressBarMaximum;
+    private float currentXP;
+    private float xpToNextLevel;
+    private float lerpDuration;
+
+    private float progressAmount; 
     
-    private float progressAmount;
-
-    private float chipSpeed;
-    private float lerpTimer;
-    private float percentComplete;
+    
+    
 
     
 
 
-    float lerpDuration = 3;
-    float startValue = 0;
-    float endValue = 5;
+    
+    float startValue = 0.1f;
+    float endValue = 0.5f;
     float valueToLerp;
 
     // Start is called before the first frame update
     void Start()
     {
-        barSprite = (RectTransform)transform.Find("Bar").transform.Find("BarSprite");
-        progressBarCurrent = .1f;
 
-        barSprite.GetComponent<Image>().fillAmount = progressBarCurrent;
+        _renderer = transform.Find("Bar").transform.Find("BarSprite").GetComponent<Image>();
+        _renderer.material.SetFloat("_FillRate", startValue);
 
-        
-        
+        //lerpDuration = 3;
+
+        //_renderer = transform.Find("Bar").transform.Find("BarSprite").GetComponent<Renderer>();
+        //progressBarCurrent = .1f;
+
+        //barSprite.GetComponent<Image>().fillAmount = progressBarCurrent;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         if (Input.GetKeyDown(KeyCode.S))
         {
             StartCoroutine(Lerp());
-
+            
 
 
 
@@ -66,10 +75,10 @@ public class ProgressBar : MonoBehaviour
     IEnumerator Lerp()
     {
         float timeElapsed = 0;
-
+        float lerpDuration = 2;
         while (timeElapsed < lerpDuration)
         {
-            barSprite.GetComponent<Image>().fillAmount = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            _renderer.material.SetFloat("_FillRate", Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration));
             timeElapsed += Time.deltaTime;
 
             yield return null;
